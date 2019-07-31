f5dee02ff657de3693eccb58a09476516ff75a15
terryllowery <terry@lowerytech.net>
2019-07-31 00:43:06 -0500

    init commit

---
+++ src/main/java/net/lowerytech/msscbrewmicro/MsscBrewMicroApplication.java
@@ -0,0 +1,13 @@
+package net.lowerytech.msscbrewmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewMicroApplication.class, args);
+	}
+
+}
