b4c96a09645eda53e669b0c0d89b88f834ac56c4
terryllowery <terry@lowerytech.net>
2019-07-31 00:52:04 -0500

    added beer dto

---
+++ src/main/java/net/lowerytech/msscbrewmicro/web/model/BeerDTO.java
@@ -0,0 +1,20 @@
+package net.lowerytech.msscbrewmicro.web.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+    private UUID id;
+    private String beerName;
+    private String style;
+    private Long upc;
+}
